69b59c2eae39549b5c43f2345c9c913927d3184f
usmansabir98 <usmansabir98@hotmail.com>
2019-05-31 00:27:29 +0500

    Add List of Cards from robots data

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({id, name, email}) => {
         <div className="bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${id}?size=200x200`} alt="robot" /> 
             <div>
-                <h2>{name}</h2>
+                <h4>{name}</h4>
                 <p>{email}</p>
             </div>
         </div>
---
+++ src/CardList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+
+    const cardArray = robots.map((robot) => {
+        return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />
+    })
+
+    return (
+        <div>
+            {cardArray}
+        </div> 
+        
+    );
+}
+
+export default CardList;
