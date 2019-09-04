332fcb42910066dc932dacfe62ea62e89b61bf5b
usmansabir98 <usmansabir98@hotmail.com>
2019-09-04 18:20:27 +0500

    Updates

---
+++ precache-manifest.e44504be70eeb5b5a919679b47b4a6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb9da6aaaa57072b008e9f4bb270969",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d6737b2df70835c8236d",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "69ffad696bb8dbfe7d20",
+    "url": "/robofriends/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "d6737b2df70835c8236d",
+    "url": "/robofriends/static/js/2.f8172335.chunk.js"
+  },
+  {
+    "revision": "69ffad696bb8dbfe7d20",
+    "url": "/robofriends/static/js/main.25871901.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  }
+]);
